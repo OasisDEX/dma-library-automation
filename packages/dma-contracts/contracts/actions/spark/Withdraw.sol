e697ff475b1075fb25daaa174a1248aa1b43439d
James Tuckett <james@tuckett.me>
2024-02-26 08:01:56 +0000

    refactor: Update contract inheritance and storage usage

--- packages/dma-contracts/contracts/actions/spark/Withdraw.sol
+++ packages/dma-contracts/contracts/actions/spark/Withdraw.sol
@@ -2,20 +2,21 @@
 pragma solidity ^0.8.15;
 
 import { Executable } from "../common/Executable.sol";
-import { Write, UseStore } from "../common/UseStore.sol";
-import { OperationStorage } from "../../core/OperationStorage.sol";
+import { UseStorageSlot, StorageSlot, Write, Read } from "../../libs/UseStorageSlot.sol";
 import { WithdrawData } from "../../core/types/Spark.sol";
 import { SPARK_LENDING_POOL } from "../../core/constants/Spark.sol";
 import { IPool } from "../../interfaces/spark/IPool.sol";
+import { ServiceRegistry } from "../../core/ServiceRegistry.sol";
+import { UseRegistry } from "../../libs/UseRegistry.sol";
 
 /**
  * @title Withdraw | Spark Action contract
  * @notice Withdraw collateral from Spark's lending pool
  */
-contract SparkWithdraw is Executable, UseStore {
-  using Write for OperationStorage;
+contract SparkWithdraw is Executable, UseStorageSlot, UseRegistry {
+  using Write for StorageSlot.TransactionStorage;
 
-  constructor(address _registry) UseStore(_registry) {}
+  constructor(address _registry) UseRegistry(ServiceRegistry(_registry)) {}
 
   /**
    * @param data Encoded calldata that conforms to the WithdrawData struct
