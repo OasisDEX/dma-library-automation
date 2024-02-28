b49b008dd4e50914dd4e1f78119ae589153d8a43
James Tuckett <james@tuckett.me>
2024-02-28 10:14:52 +0000

    refactor: Update collectFee function signature and argument

--- packages/dma-library/src/operations/aave/borrow/v3/withdraw-to-debt.ts
+++ packages/dma-library/src/operations/aave/borrow/v3/withdraw-to-debt.ts
@@ -29,9 +29,13 @@ export const withdrawToDebt: AaveV3WithdrawToDebtOperation = async args => {
     to: args.proxy,
   })
 
-  const collectFeeAfterWithdraw = actions.common.collectFee(args.network, {
-    asset: args.collateralTokenAddress,
-  })
+  const collectFeeAfterWithdraw = actions.common.collectFee(
+    args.network,
+    {
+      asset: args.collateralTokenAddress,
+    },
+    [1],
+  )
 
   const swapCollateralTokensForDebtTokens = actions.common.swap(network, {
     fromAsset: args.collateralTokenAddress,
