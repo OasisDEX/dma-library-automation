0da7250bb1f9bb5d6a04a2f23244a6388b36c214
James Tuckett <james@tuckett.me>
2024-02-28 13:21:59 +0000

    feat: Add Aave borrow withdraw strategy

---
+++ packages/dma-library/src/strategies/aave/borrow/withdraw/withdraw.ts
@@ -0,0 +1,17 @@
+
+
+
+export const withdraw: any = async () => {
+  console.log("withdrawing....")
+  // Pass args
+    // oraclePrice
+    // targetLTV
+    // shouldWithdrawToDebt
+
+  // Get Debt & Coll from protocol
+  // Calculate current LTV
+  // Calculate how much Coll to withdraw
+
+  // If withdrawing to debt calculate swap
+
+}
