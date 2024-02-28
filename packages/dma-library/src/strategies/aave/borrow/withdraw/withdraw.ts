c773556b9254a184ddba7e8ab05c2619c19e3cd5
James Tuckett <james@tuckett.me>
2024-02-28 14:11:48 +0000

    feat: Add Aave V3 withdraw function and dependencies

--- packages/dma-library/src/strategies/aave/borrow/withdraw/withdraw.ts
+++ packages/dma-library/src/strategies/aave/borrow/withdraw/withdraw.ts
@@ -1,17 +1,86 @@
+import { ZERO } from '@dma-common/constants'
+import { WithV3Protocol } from '@dma-library/types/aave/protocol'
+import * as Strategies from '@dma-library/types/strategies'
+import * as StrategyParams from '@dma-library/types/strategy-params'
+import BigNumber from 'bignumber.js'
 
+import { withdraw as withdrawOp } from '../../../../operations/aave/borrow/v3/withdraw'
 
+export type AaveV3WithdrawArgs = {
+  oraclePrice: BigNumber
+  targetLTV: BigNumber
+  shouldWithdrawToDebt: boolean
+}
 
-export const withdraw: any = async () => {
-  console.log("withdrawing....")
-  // Pass args
-    // oraclePrice
-    // targetLTV
-    // shouldWithdrawToDebt
+export type AaveV3WithdrawDependencies = Omit<
+  StrategyParams.WithAaveLikeStrategyDependencies,
+  'protocolType'
+> &
+  WithV3Protocol
 
-  // Get Debt & Coll from protocol
-  // Calculate current LTV
-  // Calculate how much Coll to withdraw
+export type AaveV3Withdraw = (
+  args: AaveV3WithdrawArgs,
+  dependencies: Omit<AaveV3WithdrawDependencies, 'protocol'>,
+) => Promise<Strategies.IStrategy>
 
+export const withdraw: AaveV3Withdraw = async (args, dependencies) => {
+  const currentPosition = dependencies.currentPosition
+
+  const amountToWithdraw = determineWithdrawalAmount(
+    currentPosition.debt.amount,
+    currentPosition.collateral.amount,
+    args.targetLTV,
+    args.oraclePrice,
+  )
+
+  const collateralTokenSymbol = currentPosition.collateral.symbol
+  const debtTokenSymbol = currentPosition.debt.symbol
+  await withdrawOp({
+    withdrawAmount: amountToWithdraw,
+    collateralTokenAddress: getTokenAddressFromDependencies(
+      dependencies,
+      currentPosition.collateral.symbol,
+    ),
+    collateralIsEth: collateralTokenSymbol === 'ETH',
+    // debtTokenAddress: getTokenAddressFromDependencies(dependencies, debtTokenSymbol),
+    // debtIsEth: debtTokenSymbol === 'ETH',
+    proxy: dependencies.proxy,
+    addresses: dependencies.addresses,
+    network: dependencies.network,
+  })
   // If withdrawing to debt calculate swap
+}
 
+function determineWithdrawalAmount(
+  existingDebt: BigNumber,
+  existingCollateral: BigNumber,
+  targetLTV: BigNumber,
+  oraclePrice: BigNumber,
+) {
+  // nextCollateral = existingDebt / (targetLTV * oraclePrice)
+  const nextCollateral = existingDebt.div(targetLTV.times(oraclePrice))
+  const amountToWithdraw = existingCollateral.minus(nextCollateral)
+
+  if (amountToWithdraw.lte(ZERO)) {
+    console.debug('next-collateral', nextCollateral.toString())
+    console.debug('amount-to-withdraw', amountToWithdraw.toString())
+    throw new Error('Cannot withdraw zero or less')
+  }
+
+  return amountToWithdraw
+}
+
+function getTokenAddressFromDependencies(
+  deps: Omit<AaveV3WithdrawDependencies, 'protocol'>,
+  symbol: string,
+) {
+  const address = deps.addresses.tokens[symbol]
+
+  if (!address) {
+    console.debug('symbol:', symbol)
+    console.debug('tokens:', deps.addresses.tokens)
+    throw new Error('Could not get address from position token symbol')
+  }
+
+  return address
 }
