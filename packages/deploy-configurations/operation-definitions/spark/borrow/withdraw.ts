3a15d8fc6b18a715ba87f752d6fbc76629793918
James Tuckett <james@tuckett.me>
2024-02-28 09:03:27 +0000

    feat: Add Aave V3 withdraw and withdraw-to-debt operations

---
+++ packages/deploy-configurations/operation-definitions/spark/borrow/withdraw.ts
@@ -0,0 +1,30 @@
+
+import { loadContractNames, OPERATION_NAMES } from '@deploy-configurations/constants'
+import { Network } from '@deploy-configurations/types/network'
+import { getActionHash } from '@deploy-configurations/utils/action-hash'
+
+export function getAaveV3WithdrawOperationDefinition(network: Network) {
+    const SERVICE_REGISTRY_NAMES = loadContractNames(network)
+
+    return {
+        name: OPERATION_NAMES.aave.v3.WITHDRAW,
+        actions: [
+            {
+                hash: getActionHash(SERVICE_REGISTRY_NAMES.aave.v3.WITHDRAW),
+                optional: false,
+            },
+            {
+                hash: getActionHash(SERVICE_REGISTRY_NAMES.common.COLLECT_FEE),
+                optional: false,
+            },
+            {
+                hash: getActionHash(SERVICE_REGISTRY_NAMES.common.UNWRAP_ETH),
+                optional: false,
+            },
+            {
+                hash: getActionHash(SERVICE_REGISTRY_NAMES.common.RETURN_FUNDS),
+                optional: false,
+            },
+        ],
+    }
+}
