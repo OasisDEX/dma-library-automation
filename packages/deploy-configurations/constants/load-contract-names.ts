eb6c0f5b09b1990d828a4c69d84202f6b570c3a2
James Tuckett <james@tuckett.me>
2024-02-29 10:01:43 +0000

    fix: Update CollectFee address and history addresses

--- packages/deploy-configurations/constants/load-contract-names.ts
+++ packages/deploy-configurations/constants/load-contract-names.ts
@@ -55,7 +55,7 @@ export const SERVICE_REGISTRY_NAMES = {
     WRAP_ETH: 'WrapEth_6',
     UNWRAP_ETH: 'UnwrapEth_6',
     RETURN_FUNDS: 'ReturnFunds_6',
-    COLLECT_FEE: 'CollectFee_2',
+    COLLECT_FEE: 'CollectFee_3',
     POSITION_CREATED: 'PositionCreated',
     ACCOUNT_GUARD: 'AccountGuard',
     ACCOUNT_FACTORY: 'AccountFactory',
